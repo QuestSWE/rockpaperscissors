b51e0c2929910f5410271de9f8a29eb24443685d
Quest {SWE} <thacrank0001@gmail.com>
2024-07-02 14:59:19 -0400

    UPDATED entire function logics of getComputerChoice and getHumanChoice. REMOVED all the if statement of both functions. ADDED a humanChoice object to the getHumanChoice func which assign a string to an int.

--- javascript.js
+++ javascript.js
@@ -1,51 +1,34 @@
-// GET computerChoice
-// GENERATE random number between 1 and 3
-// ASSIGN value to result
-// IF result is 1 THEN return "rock"
-// ELSE IF result is 2 THEN return "paper"
-// ELSE return "scissors"
+const humanScore = 0;
+const computerScore = 0;
 
-// GET humanChoice
-// DISPLAY to user choice between rock paper or scissors
-// OBTAIN user string input choice
-// IF user inputs is NOT one of the choices
-// THEN DISPLAY a message to user that its not a valid input, repeat choices
-// RETURN valid string input
-
-// function getComputerChoice() {
-//   // Generates a number between 1 and 3
-//   let result = Math.floor(Math.random() * 3) + 1;
-//   console.log(result);
-//   if (result === 1) {
-//     return console.log("Rock");
-//   } else if (result === 2) {
-//     return console.log("Paper");
-//   } else return console.log("Scissors");
-// }
-// getComputerChoice();
-
-function getHumanChoice(string) {
-  let validChoice = false;
-  // console.log(validChoice);
-
-  while (!validChoice) {
-    let choices = prompt("Rock, Paper or Scissors?");
-
-    if (choices.toLowerCase() === "rock") {
-      console.log("Rock");
-      validChoice = true;
-      return alert("You chose Rock!");
-    } else if (choices.toLowerCase() === "paper") {
-      console.log("Paper");
-      validChoice = true;
-      return alert("You chose Paper!");
-    } else if (choices.toLowerCase() === "scissors") {
-      console.log("Scissors");
-      validChoice = true;
-      return alert("You chose Scissors!");
-    } else
-      alert("Invalid choice, please choose between Rock, Paper or Scissors");
-  }
+function getComputerChoice(n) {
+  // Generates a number between 1 and 3
+  let result = Math.floor(Math.random() * 3) + 1;
+  console.log(result);
 }
 
+function getHumanChoice(string) {
+  let choices = prompt("Rock, Paper or Scissors?");
+
+  let humanChoice = {
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+  };
+
+  let lowerCaseInput = choices.toLowerCase();
+
+  let humanChoices = humanChoice[lowerCaseInput];
+  return console.log(humanChoices);
+}
+
+function playRound(getHumanChoice, getComputerChoice) {
+  let numToStr = {
+    1: "Rock",
+    2: "Paper",
+    3: "Scissors",
+  };
+}
+getComputerChoice();
 getHumanChoice();
+// playRound();
