432b6380050c921180136b00de083d16e75c52c2
Quest {SWE} <thacrank0001@gmail.com>
2024-07-02 18:47:13 -0400

    UPDATED func playRound, the logics of the rock paper and scissors is functional. ADDED score update to humanScore and computerScore. ADDED amessage when the scores are displayed in the console.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   // Generates a number between 1 and 3
@@ -21,9 +21,65 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  
-  // console.log(humanChoice,  `CPU ${computerChoice} `);
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
+    console.log("You Lose! Paper beats rock!");
+    console.log(
+      `Human Score is Now ---> ${humanScore} |*| Computer Score is Now ---> ${computerScore}`
+    );
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    console.log("You Won! Rock beats Scissors!");
+    console.log(
+      `Human Score is Now ---> ${humanScore} |*| Computer Score is Now ---> ${computerScore}`
+    );
+  } else if (humanChoice === "rock" && computerChoice === "rock") {
+    console.log("It's a Draw!");
+    console.log(
+      `Human Score is Still ---> ${humanScore} |*| Computer Score is Still ---> ${computerScore}`
+    );
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    console.log("You Won! Paper beats Rock!");
+    console.log(
+      `Human Score is Now ---> ${humanScore} |*| Computer Score is Now ---> ${computerScore}`
+    );
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
+    console.log("You Lose! Scissors beats Paper!");
+    console.log(
+      `Human Score is Now ---> ${humanScore} |*| Computer Score is Now ---> ${computerScore}`
+    );
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    console.log("It's a Draw!");
+    console.log(
+      `Human Score is Still ---> ${humanScore} |*| Computer Score is Still ---> ${computerScore}`
+    );
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
+    console.log("You Lose! Rock beats Scissors");
+    console.log(
+      `Human Score is Now ---> ${humanScore} |*| Computer Score is Now ---> ${computerScore}`
+    );
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    console.log("You Won! Scissors beats Paper");
+    console.log(
+      `Human Score is Now ---> ${humanScore} |*| Computer Score is Now ---> ${computerScore}`
+    );
+  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+    console.log("It's a Draw!");
+    console.log(
+      `Human Score is Still ---> ${humanScore} |*| Computer Score is Still ---> ${computerScore}`
+    );
+  }
+
+  console.log(
+    `User Choice: ${humanChoice},  Computer Choice: ${computerChoice}`
+  );
 }
+humanScore = 0;
+computerScore = 0;
 
 const humanSelection = getHumanChoice();
 // console.log(humanSelection);
