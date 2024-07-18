c2cc374c425bd2cb28b760ebd6bf3ec384f0fa93
Quest {SWE} <thacrank0001@gmail.com>
2024-07-18 15:49:13 -0400

    REFACTORED playRound function, it is now more readable. Small update made to README.md

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 This is just a little project to put in practice what I learned so far.
-***************************************
+
+---
+
 THIS GAME MUST BE PLAYED IN THE CONSOLE
-***************************************
+
+---
--- javascript.js
+++ javascript.js
@@ -3,7 +3,8 @@ let roundValue = [];
 let humanScore = 0;
 let computerScore = 0;
 let currentRound = 0;
-let humanChoice = '';
+let computerChoice;
+let humanChoice;
 // Function to get the computer's choice
 // Randomly returns "rock", "paper", or "scissors"
 function getComputerChoice() {
@@ -18,68 +19,46 @@ function getComputerChoice() {
 }
 
 // Function to get the human's choice
-// Prompts the user for input and validates it
-// If invalid input is provided, the game restarts
-// Returns null if the prompt is canceled
 function getHumanChoice() {
   const buttons = document.querySelectorAll("button");
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      if (button.id === "rock") {
-        humanChoice = button.id;
-        console.log(humanChoice);
-        return humanChoice;
-      } else if (button.id === "paper") {
-        console.log("paper it is");
-      } else console.log("scissors");
+      humanChoice = button.id;
+      console.log("human choice", humanChoice);
+
+      computerChoice = getComputerChoice();
+      console.log("computer choice", computerChoice);
+      playRound(humanChoice, computerChoice);
     });
   });
 }
 
 // Function to play a single round
 // Takes human and computer choices as parameters
-// Updates scores and logs the outcome to the console
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore++;
-    console.log("Computer Chose Paper. You Lose! Paper Beats Rock!");
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+  if (humanChoice === computerChoice) {
+    console.log(`IT'S A TIE! YOU BOTH CHOSE ${humanChoice.toUpperCase()}!`);
+    console.log("human score", humanScore, "computer score", computerScore);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
     humanScore++;
-    console.log("Computer Chose Scissors. You Won! Rock beats Scissors!");
-  } else if (humanChoice === "rock" && computerChoice === "rock") {
-    console.log("Computer Also Chose Rock. It's a Tie!");
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    humanScore++;
-    console.log("Computer Chose Rock. You Won! Paper beats Rock!");
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log(
+      `YOU WON! ${humanChoice.toUpperCase()} BEATS ${computerChoice.toUpperCase()}!`
+    );
+    console.log("human score", humanScore);
+  } else {
+    console.log(
+      `YOU LOSE! ${computerChoice.toUpperCase()} BEATS ${humanChoice.toUpperCase()}!`
+    );
     computerScore++;
-    console.log("Computer Chose Scissors. You Lose! Scissors beats Paper!");
-  } else if (humanChoice === "paper" && computerChoice === "paper") {
-    console.log("Computer Also Chose Paper. It's a Tie!");
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    computerScore++;
-    console.log("Computer Chose Rock. You Lose! Rock beats Scissors");
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    humanScore++;
-    console.log("Computer Chose Paper. You Won! Scissors beats Paper");
-  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    console.log("Computer Also Chose Scissors. It's a Tie!");
+    console.log("computer score", computerScore);
   }
 }
-
-// const buttons = document.querySelectorAll("button");
-
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     if (button.id === 'rock') {
-//       console.log('button is rock');
-//     } else if (button.id === "paper") {
-//       console.log("paper it is");
-//     } else console.log("scissors");
-//   });
-// });
-
+getHumanChoice();
 // Main function to play the game
 // function playGame() {
 //   // playAgain();
@@ -96,4 +75,3 @@ function playRound(humanChoice, computerChoice) {
 // }
 
 // playGame();
-getHumanChoice();
